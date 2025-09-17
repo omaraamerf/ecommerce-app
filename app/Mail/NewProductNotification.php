3cc29494a1a131346fa7b5f016aa5989f2e1fffd
omaraamerf <omarsheleh949@gmail.com>
2025-09-17 12:15:38 +0300

    qeue

--- app/Mail/NewProductNotification.php
+++ app/Mail/NewProductNotification.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NewProductNotification extends Mailable
+class NewProductNotification extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
